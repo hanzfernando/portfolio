b08363c4be77872228866b68ba4a4d35127f451a
hanzfernando <fernandohanz23@gmail.com>
2025-05-12 18:45:32 +0800

    tried fixing image positioning on smaller devices;

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -7,29 +7,31 @@ const AboutPage = () => {
     return (
         <>
             <BurgerNav />
-            <div className="flex flex-col justify-center items-center h-full px-4 ">
-                <div className="mt-[-10vh] mb-7 max-h-[20%] w-48 h-48">
-                    <img src={profile} alt="Hanz Fernando" className="max-h-full rounded-full object-cover" />
+            <div className="flex flex-col justify-center items-center h-full px-4">
+                <div className="mb-7 w-48 h-48 md:w-64 md:h-64 lg:w-80 lg:h-80">
+                    <img 
+                        src={profile} 
+                        alt="Hanz Fernando" 
+                        className="w-full h-full rounded-full object-cover" 
+                    />
                 </div>
 
                 <h1 className="text-[1.8em] leading-[2em] text-red text-center font-bold text-[#c5c5c5]">
                     Hello, I'm Hanz Fernando
                 </h1>
 
-                <p className="text-center tex-justify text-[1.05em] leading-[2em] text-[#949494] font-poppins max-w-xl">
-                I'm a 20-year-old college student with a passion for web development. Feel free to explore my{' '}
-                <Link to={'/projects'} className="font-bold no-underline text-[#c5c5c5]">
-                    previous projects
-                </Link>{' '}
-                to get a sense of my work. I am available from UTC-5 to UTC-10.{' '}
-                <span className="font-bold no-underline text-[#c5c5c5]">
-                    fernandohanz23@gmail.com
-                </span>
+                <p className="text-center text-justify text-[1.05em] leading-[2em] text-[#949494] font-poppins max-w-xl">
+                    I'm a 20-year-old college student with a passion for web development. Feel free to explore my{' '}
+                    <Link to={'/projects'} className="font-bold no-underline text-[#c5c5c5]">
+                        previous projects
+                    </Link>{' '}
+                    to get a sense of my work. I am available from UTC-5 to UTC-10.{' '}
+                    <span className="font-bold no-underline text-[#c5c5c5]">
+                        fernandohanz23@gmail.com
+                    </span>
                 </p>
-
-
-
             </div>
+
             <div className="hide-on-small">
                 <PageNavigation nextPage={'/projects'} nextPageName={'projects'} className="block [@media(min-width:1400px)]:hidden"/>
             </div>
